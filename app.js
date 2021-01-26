0fd9f0e5c2286061e23357e7d3cb233a7e33a46a
Gerald Ventura <58758273+Vent2@users.noreply.github.com>
2021-01-25 19:39:31 -0500

    Comment
    Explaing what that function is doing

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ const logo = document.querySelectorAll('#logo path');
 for (let i = 0; i < logo.length; i++) {
     console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
 }
+
+// This is to figure out the length for each letter stroke-dasharray
